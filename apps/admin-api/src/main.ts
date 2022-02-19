2a8dde38c98c9500dd0027a37cf4e4b899b9f15f
Sham <realtimestack@gmail.com>
2022-02-19 16:27:29 +0800

    add fastify adapter

--- apps/admin-api/src/main.ts
+++ apps/admin-api/src/main.ts
@@ -5,18 +5,39 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyStatic from 'fastify-static';
+import fastifyMultipart from 'fastify-multipart';
+import fastifyCors from 'fastify-cors';
+import { join } from 'path';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const adapter = new FastifyAdapter();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter
   );
+  // const globalPrefix = 'api';
+  // app.setGlobalPrefix(globalPrefix);
+  const port = process.env.ADMIN_API_PORT || 7001;
+  app.register(fastifyCors);
+  app.register(fastifyMultipart);
+  app.register(fastifyStatic, {
+    prefix: '/uploads/',
+    root: join(process.cwd(), 'uploads'),
+  });
+  await app.listen(port, '0.0.0.0', () => {
+    Logger.log(`Listening at http://localhost:${port}`, 'Admin API');
+  });
+  // await app.listen(port);
+  // Logger.log(
+  //   `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  // );
 }
 
 bootstrap();
