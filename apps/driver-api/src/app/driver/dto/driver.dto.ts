5df414da4cba23a8be4f58aa91c7290ee5a6205b
Sham <realtimestack@gmail.com>
2022-10-02 08:11:58 +0800

    fixbug driver no media

--- apps/driver-api/src/app/driver/dto/driver.dto.ts
+++ apps/driver-api/src/app/driver/dto/driver.dto.ts
@@ -14,11 +14,11 @@ import { DriverStatus } from '@vroom/database/enums/driver-status.enum';
 import { UserContext } from '../../auth/authenticated-user';
 import { CarModelDTO } from './car-model.dto';
 import { CarColorDTO } from './car-color.dto';
-// import { OrderDTO } from "../../order/dto/order.dto";
+import { OrderDTO } from '../../order/dto/order.dto';
 import { SortDirection } from '@ptc-org/nestjs-query-core';
 import { OrderStatus } from '@vroom/database/enums/order-status.enum';
-// import { MediaDTO } from "../../upload/media.dto";
-// import { DriverWalletDTO } from "../../wallet/dto/driver-wallet.dto";
+import { MediaDTO } from '../../upload/media.dto';
+import { DriverWalletDTO } from '../../wallet/dto/driver-wallet.dto';
 
 @ObjectType('Driver')
 @Authorize({
@@ -27,7 +27,7 @@ import { OrderStatus } from '@vroom/database/enums/order-status.enum';
 @BeforeFindOne((input: FindOneArgsType, context: UserContext) => {
   return { id: context.req.user.id };
 })
-// @UnPagedRelation('documents', () => MediaDTO, { disableRemove: true })
+@UnPagedRelation('documents', () => MediaDTO, { disableRemove: true })
 @Relation('car', () => CarModelDTO, {
   nullable: true,
   disableRemove: true,
@@ -38,10 +38,51 @@ import { OrderStatus } from '@vroom/database/enums/order-status.enum';
   disableRemove: true,
   disableUpdate: true,
 })
-// @Relation('media', () => MediaDTO, { nullable: true, disableRemove: true, disableUpdate: true })
-// @OffsetConnection('orders', () => OrderDTO, { maxResultsSize: 1, defaultFilter: {status: {in: [OrderStatus.DriverAccepted, OrderStatus.Arrived, OrderStatus.Started, OrderStatus.WaitingForPostPay]}}, defaultSort: [{field: 'id', direction: SortDirection.DESC}], defaultResultSize: 1, disableRemove: true, disableUpdate: true})
-// @UnPagedRelation('currentOrders', () => OrderDTO, { maxResultsSize: 1, defaultFilter: {status: {in: [OrderStatus.DriverAccepted, OrderStatus.Arrived, OrderStatus.Started, OrderStatus.WaitingForPostPay]}}, defaultSort: [{field: 'id', direction: SortDirection.DESC}], defaultResultSize: 1, relationName: 'orders', disableRemove: true, disableUpdate: true})
-// @UnPagedRelation('wallet', () => DriverWalletDTO, { relationName: 'wallet', disableRemove: true, disableUpdate: true })
+@Relation('media', () => MediaDTO, {
+  nullable: true,
+  disableRemove: true,
+  disableUpdate: true,
+})
+@OffsetConnection('orders', () => OrderDTO, {
+  maxResultsSize: 1,
+  defaultFilter: {
+    status: {
+      in: [
+        OrderStatus.DriverAccepted,
+        OrderStatus.Arrived,
+        OrderStatus.Started,
+        OrderStatus.WaitingForPostPay,
+      ],
+    },
+  },
+  defaultSort: [{ field: 'id', direction: SortDirection.DESC }],
+  defaultResultSize: 1,
+  disableRemove: true,
+  disableUpdate: true,
+})
+@UnPagedRelation('currentOrders', () => OrderDTO, {
+  maxResultsSize: 1,
+  defaultFilter: {
+    status: {
+      in: [
+        OrderStatus.DriverAccepted,
+        OrderStatus.Arrived,
+        OrderStatus.Started,
+        OrderStatus.WaitingForPostPay,
+      ],
+    },
+  },
+  defaultSort: [{ field: 'id', direction: SortDirection.DESC }],
+  defaultResultSize: 1,
+  relationName: 'orders',
+  disableRemove: true,
+  disableUpdate: true,
+})
+@UnPagedRelation('wallet', () => DriverWalletDTO, {
+  relationName: 'wallet',
+  disableRemove: true,
+  disableUpdate: true,
+})
 export class DriverDTO {
   @IDField(() => ID)
   id!: number;
